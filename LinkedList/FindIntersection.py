146946fdf72a704aacf18d5702d142d1b4951f33
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-12-11 13:22:22 -0500

    _

---
+++ LinkedList/FindIntersection.py
@@ -0,0 +1,11 @@
+'''
+Given two singly linked list that intersect at some point find 
+the interecting no
+de assume the list are non-cyclical. 
+
+A = 3 -> 7 -\ 
+             8 -> 10
+B = 99 - 1 -/
+
+return 8
+'''
