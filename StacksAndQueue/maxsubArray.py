da80298f4305481afb451cf9b1e62a2470e089b9
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-12-14 15:03:18 -0500

    using queue

--- StacksAndQueue/maxsubArray.py
+++ StacksAndQueue/maxsubArray.py
@@ -8,3 +8,17 @@ Compute the maximum of k-length subarrays
 def max_of_subArray(lst, k):
     for i in range(len(lst)-k+1):
         print(max(lst[i:i+k]))
+
+
+from collections import deque
+def maxOfSubarray(lst, k):
+    q = deque()
+    for i in range(k):
+        print(q)
+        while q and lst[i] >= lst[q[-1]]:
+            q.pop()
+        q.append()
+    print(q)
+
+
+print(maxOfSubarray([10,5,2,7,8,7],3))
